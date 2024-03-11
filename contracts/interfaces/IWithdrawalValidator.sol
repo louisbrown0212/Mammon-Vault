556c316f49f81425c839dd5b63b6955a2fbfadde
Louis Brown <louisbrown0212@gmail.com>
2024-03-11 08:26:06 -0400

    added example contract withdrawal validator + tests + solidity ci

---
+++ contracts/interfaces/IWithdrawalValidator.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.6;
+
+/// @notice Represents the withdrawal conditions for a vault.
+/// @dev Should be extended by vault owner or manager, deployed and attached
+/// to a vault instance.
+interface IWithdrawalValidator {
+    /// @notice Determines how much of each token could be withdrawn under
+    /// current conditions.
+    /// @return token1Amount, token2Amount The quantity of each token that
+    /// can be withdrawn from the vault.
+    /// @dev Token quantity value should be interpreted with the same
+    /// decimals as the token ERC20 balance.
+    function allowance() external view returns (uint256, uint256);
+}
