eee4712d17fb23a2e1cede54092ae0bb1981d6b1
Louis Brown <louisbrown0212@gmail.com>
2024-03-13 04:06:02 -0400

    solidity tweaks

--- test/WithdrawalValidator.ts
+++ test/WithdrawalValidator.ts
@@ -19,13 +19,18 @@ describe("Withdrawal Validator", function () {
 
   describe("Permissive Withdrawal Validator", function () {
     beforeEach(async function () {
-      const validatorArtifact: Artifact = await hre.artifacts.readArtifact("PermissiveWithdrawalValidator");
-      this.validator = <PermissiveWithdrawalValidator>await deployContract(this.signers.admin, validatorArtifact);
+      const validatorArtifact: Artifact = await hre.artifacts.readArtifact(
+        "PermissiveWithdrawalValidator",
+      );
+      this.validator = <PermissiveWithdrawalValidator>(
+        await deployContract(this.signers.admin, validatorArtifact)
+      );
     });
 
     it("should return the full withdrawal allowance", async function () {
       const uint256_max: BigNumber = BigNumber.from(2).pow(256).sub(1);
-      const tokenAllowance: [BigNumber, BigNumber] = await this.validator.allowance();
+      const tokenAllowance: [BigNumber, BigNumber] =
+        await this.validator.allowance();
       expect(tokenAllowance[0]).to.equal(uint256_max);
       expect(tokenAllowance[1]).to.equal(uint256_max);
     });
