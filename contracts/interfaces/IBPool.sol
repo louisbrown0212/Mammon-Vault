aa9013ba0a67aa6d83bf37ca493292e2b4efba50
Louis Brown <louisbrown0212@gmail.com>
2024-04-29 09:53:38 -0400

    Refactoring and various fixes

--- contracts/interfaces/IBPool.sol
+++ contracts/interfaces/IBPool.sol
@@ -1,19 +1,41 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.8.6;
+pragma solidity >=0.8.7;
 
 interface IBPool {
-    function bind(address token, uint balance, uint denorm) external;
-    function rebind(address token, uint balance, uint denorm) external;
+    function bind(
+        address token,
+        uint256 balance,
+        uint256 denorm
+    ) external;
+
+    function rebind(
+        address token,
+        uint256 balance,
+        uint256 denorm
+    ) external;
+
     function gulp(address token) external;
 
-    function setSwapFee(uint swapFee) external;
+    function finalize() external;
+
+    function setSwapFee(uint256 swapFee) external;
+
     function setPublicSwap(bool publicSwap) external;
 
     function isPublicSwap() external view returns (bool);
-    function getSwapFee() external view returns (uint);
-    function getBalance(address token) external view returns (uint);
-    function getDenormalizedWeight(address token) external view returns (uint);
-    function MIN_WEIGHT() external view returns (uint);
-    function MAX_WEIGHT() external view returns (uint);
-    function MIN_BALANCE() external view returns (uint);
+
+    function getSwapFee() external view returns (uint256);
+
+    function getBalance(address token) external view returns (uint256);
+
+    function getDenormalizedWeight(address token)
+        external
+        view
+        returns (uint256);
+
+    function MIN_WEIGHT() external view returns (uint256);
+
+    function MAX_WEIGHT() external view returns (uint256);
+
+    function MIN_BALANCE() external view returns (uint256);
 }
