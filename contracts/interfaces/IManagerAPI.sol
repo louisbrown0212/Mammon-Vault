59f53c1af1cb15bafcfe5fb06ea3ae43acf51591
Louis Brown <louisbrown0212@gmail.com>
2024-04-30 03:36:15 -0400

    add interfaces and manager role

---
+++ contracts/interfaces/IManagerAPI.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.7;
+
+interface IManagerAPI {
+    function updateWeightsGradually(
+        uint256 weight0,
+        uint256 weight1,
+        uint256 startBlock,
+        uint256 endBlock
+    ) external;
+
+    function pokeWeights() external;
+
+    function setSwapFee(uint256 newSwapFee) external;
+}
