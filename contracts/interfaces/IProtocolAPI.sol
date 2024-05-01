f8864451bbe955c866f7f0960c87e66e50124792
Louis Brown <louisbrown0212@gmail.com>
2024-05-01 09:53:38 -0400

    Refactoring and various fixes

--- contracts/interfaces/IProtocolAPI.sol
+++ contracts/interfaces/IProtocolAPI.sol
@@ -2,6 +2,13 @@
 pragma solidity >=0.8.7;
 
 interface IProtocolAPI {
+    function initialDeposit(
+        uint256 amount0,
+        uint256 amount1,
+        uint256 weight0,
+        uint256 weight1
+    ) external;
+
     function deposit(uint256 amount0, uint256 amount1) external;
 
     function withdraw(uint256 amount0, uint256 amount1) external;
