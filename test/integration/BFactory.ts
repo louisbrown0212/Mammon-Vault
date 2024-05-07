f826ea773e15d198f97f2646e69ce8570e1ff1c4
Louis Brown <louisbrown0212@gmail.com>
2024-05-07 05:53:35 -0400

    add integration tests

---
+++ test/integration/BFactory.ts
@@ -0,0 +1,54 @@
+import hre from "hardhat";
+import { BigNumber } from "@ethersproject/bignumber";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signers";
+import {
+  IBFactory,
+  IBFactory__factory,
+  IERC20,
+  IERC20__factory
+ } from "../../typechain";
+
+let admin: SignerWithAddress;
+let user1: SignerWithAddress;
+let bFactory: IBFactory;
+let weth: IERC20, dai: IERC20;
+let ADMIN, NON_ADMIN, WETH, DAI;
+const uint256_max: BigNumber = BigNumber.from(2).pow(256).sub(1);
+
+describe("Factory", function () {
+  before(async function () {
+    const signers: SignerWithAddress[] = await hre.ethers.getSigners();
+    admin = signers[0];
+    user1 = signers[1];
+    ADMIN = await admin.getAddress();
+    NON_ADMIN = await user1.getAddress();
+
+    bFactory = IBFactory__factory.connect(
+      <string>process.env.BFACTORY,
+      admin.provider!,
+    );
+
+    weth = IERC20__factory.connect(<string>process.env.WETH, admin.provider!);
+    dai = IERC20__factory.connect(<string>process.env.DAI, admin.provider!);
+
+    WETH = weth.address;
+    DAI = dai.address;
+  });
+
+  it("should deploy balancer private pool", async function () {
+    let pool = await bFactory.connect(admin).newBPool(); // this works fine in clean room
+    let receipt = await pool.wait();
+    let LOG_NEW_POOL = receipt.events?.find(
+      event =>
+        event.topics[0] ==
+        "0x8ccec77b0cb63ac2cafd0f5de8cdfadab91ce656d262240ba8a6343bccc5f945",
+    );
+    let POOL = `0x${LOG_NEW_POOL?.topics[2].slice(26)}`;
+
+    await weth.connect(admin).approve(POOL, uint256_max);
+    await dai.connect(admin).approve(POOL, uint256_max);
+
+    await weth.connect(user1).approve(POOL, uint256_max);
+    await dai.connect(user1).approve(POOL, uint256_max);
+  });
+});
---
+++ test/utils.ts
@@ -0,0 +1,40 @@
+import { Signer } from "ethers";
+import { ethers, deployments } from "hardhat";
+import { DEFAULT_NOTICE_PERIOD, getConfig } from "../scripts/config";
+import {
+  MammonVaultV0Mainnet,
+  MammonVaultV0Mainnet__factory,
+} from "../typechain";
+
+export const deployVault = async (
+  signer: Signer,
+  token0: string,
+  token1: string,
+  manager: string,
+  validator?: string,
+  noticePeriod: number = DEFAULT_NOTICE_PERIOD,
+): Promise<MammonVaultV0Mainnet> => {
+  const chainId = (await ethers.provider.getNetwork()).chainId;
+  const config = getConfig(chainId);
+
+  const factory =
+    await ethers.getContractFactory<MammonVaultV0Mainnet__factory>(
+      config.vault,
+      {
+        libraries: {
+          SmartPoolManager: config.poolManager,
+        },
+      },
+    );
+
+  if (!validator) {
+    validator = (await deployments.get("Validator")).address;
+  }
+  return await factory
+    .connect(signer)
+    .deploy(token0, token1, manager, validator, noticePeriod);
+};
+
+export const toWei = (value: any) => {
+  return ethers.utils.parseEther(value.toString());
+}
