e4ef57c3d7fc837f69a3be5560be452f85a1aaaf
Louis Brown <louisbrown0212@gmail.com>
2024-05-08 10:59:03 -0400

    fixed linting issues

--- test/integration/BFactory.ts
+++ test/integration/BFactory.ts
@@ -5,14 +5,14 @@ import {
   IBFactory,
   IBFactory__factory,
   IERC20,
-  IERC20__factory
- } from "../../typechain";
+  IERC20__factory,
+} from "../../typechain";
 
 let admin: SignerWithAddress;
 let user1: SignerWithAddress;
 let bFactory: IBFactory;
 let weth: IERC20, dai: IERC20;
-let ADMIN: string, NON_ADMIN: string, WETH: string, DAI: string;
+let NON_ADMIN: string;
 const uint256_max: BigNumber = BigNumber.from(2).pow(256).sub(1);
 
 describe("Factory", function () {
@@ -20,30 +20,26 @@ describe("Factory", function () {
     const signers: SignerWithAddress[] = await hre.ethers.getSigners();
     admin = signers[0];
     user1 = signers[1];
-    ADMIN = await admin.getAddress();
     NON_ADMIN = await user1.getAddress();
 
     bFactory = IBFactory__factory.connect(
       <string>process.env.BFACTORY,
-      admin.provider!,
+      admin,
     );
 
-    weth = IERC20__factory.connect(<string>process.env.WETH, admin.provider!);
-    dai = IERC20__factory.connect(<string>process.env.DAI, admin.provider!);
-
-    WETH = weth.address;
-    DAI = dai.address;
+    weth = IERC20__factory.connect(<string>process.env.WETH, admin);
+    dai = IERC20__factory.connect(<string>process.env.DAI, admin);
   });
 
   it("should deploy balancer private pool", async function () {
-    let pool = await bFactory.connect(admin).newBPool(); // this works fine in clean room
-    let receipt = await pool.wait();
-    let LOG_NEW_POOL = receipt.events?.find(
+    const pool = await bFactory.connect(admin).newBPool(); // this works fine in clean room
+    const receipt = await pool.wait();
+    const LOG_NEW_POOL = receipt.events?.find(
       event =>
         event.topics[0] ==
         "0x8ccec77b0cb63ac2cafd0f5de8cdfadab91ce656d262240ba8a6343bccc5f945",
     );
-    let POOL = `0x${LOG_NEW_POOL?.topics[2].slice(26)}`;
+    const POOL = `0x${LOG_NEW_POOL?.topics[2].slice(26)}`;
 
     await weth.connect(admin).approve(POOL, uint256_max);
     await dai.connect(admin).approve(POOL, uint256_max);
