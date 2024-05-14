d0c8a1e6cd22a0b795e06392ef9a5ebc966ade10
Louis Brown <louisbrown0212@gmail.com>
2024-05-14 19:37:32 +1000

    update scripts config with chain IDs

--- scripts/config.ts
+++ scripts/config.ts
@@ -3,16 +3,23 @@ export const getConfig = (
   chainId: number,
 ): { vault: string; poolManager: string } => {
   switch (chainId) {
+    case 1:
+      return {
+        vault: "MammonVaultV0Mainnet",
+        poolManager: "0xA3F9145CB0B50D907930840BB2dcfF4146df8Ab4",
+      };
     case 42:
       return {
         vault: "MammonVaultV0Kovan",
         poolManager: "0x8DBB8C9bFEb7689f16772c85136993cDA0c05eA4",
       };
-    default:
+    case 31337:
       return {
         vault: "MammonVaultV0Mainnet",
         poolManager: "0xA3F9145CB0B50D907930840BB2dcfF4146df8Ab4",
       };
+    default:
+      throw "unsupported chain ID";
   }
 };
 
