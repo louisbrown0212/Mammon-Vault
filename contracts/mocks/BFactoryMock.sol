972c8f8be354f56a374efb268cedab612420d835
Louis Brown <louisbrown0212@gmail.com>
2024-05-16 12:47:31 -0400

    fix conflict in ERC20Mock.sol

---
+++ contracts/mocks/BFactoryMock.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.7;
+
+import "./BPoolMock.sol";
+
+/// @dev This code is not audited or tested. Please do not use in production.
+contract BFactoryMock {
+    constructor() {}
+
+    function newBPool() external returns (address) {
+        return address(new BPoolMock());
+    }
+}
