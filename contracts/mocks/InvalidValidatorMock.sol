24b999012ec342ef57702897c230cdd5f6344a9c
Louis Brown <louisbrown0212@gmail.com>
2024-05-27 00:21:09 +1000

    update ERC165 implementation

--- contracts/mocks/InvalidValidatorMock.sol
+++ contracts/mocks/InvalidValidatorMock.sol
@@ -2,22 +2,15 @@
 pragma solidity 0.8.7;
 
 import "../dependencies/openzeppelin/IERC165.sol";
-import { IWithdrawalValidator } from "../interfaces/IWithdrawalValidator.sol";
 
 /// @notice A withdrawal validator that validates withdrawals of an arbitrary size.
-contract InvalidValidatorMock is IERC165, IWithdrawalValidator {
-    uint256 public constant ANY_AMOUNT = type(uint256).max;
-
-    function allowance() external pure override returns (uint256, uint256) {
-        return (ANY_AMOUNT, ANY_AMOUNT);
-    }
-
+contract InvalidValidatorMock is IERC165 {
     function supportsInterface(bytes4 interfaceID)
         external
         view
         override
         returns (bool)
     {
-        return interfaceID != this.allowance.selector;
+        return false;
     }
 }
