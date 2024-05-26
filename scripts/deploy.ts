c4d17156fe5a2509bfa52352cf6de0025ea99488
Louis Brown <louisbrown0212@gmail.com>
2024-05-25 20:11:13 -0400

    fix conflicts in package.json

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,15 +2,62 @@
 // script in a standalone fashion through `node <script>`. When running the script with `hardhat run <script>`,
 // you'll find the Hardhat Runtime Environment's members available in the global scope.
 import { ethers } from "hardhat";
-
-import { Greeter, Greeter__factory } from "../typechain";
+import { DEFAULT_NOTICE_PERIOD, getConfig } from "../scripts/config";
+import {
+  MammonVaultV0,
+  MammonVaultV0__factory,
+  PermissiveWithdrawalValidator,
+  PermissiveWithdrawalValidator__factory,
+} from "../typechain";
 
 async function main(): Promise<void> {
-  const Greeter: Greeter__factory = await ethers.getContractFactory("Greeter");
-  const greeter: Greeter = await Greeter.deploy("Hello, Hardhat!");
-  await greeter.deployed();
+  if (!process.env.TOKEN0) {
+    console.log("Token0 is not set");
+    return;
+  }
+  if (!process.env.TOKEN1) {
+    console.log("Token1 is not set");
+    return;
+  }
 
-  console.log("Greeter deployed to: ", greeter.address);
+  const token0 = process.env.TOKEN0;
+  const token1 = process.env.TOKEN1;
+  const manager = process.env.MANAGER || ethers.constants.ZERO_ADDRESS;
+
+  console.log("Deploying vault with");
+  console.log(`Token0: ${token0}`);
+  console.log(`Token1: ${token1}`);
+  console.log(`Manager: ${manager}`);
+
+  const chainId = (await ethers.provider.getNetwork()).chainId;
+  const config = getConfig(chainId);
+
+  const validatorFactory: PermissiveWithdrawalValidator__factory =
+    await ethers.getContractFactory("PermissiveWithdrawalValidator");
+  const validator = <PermissiveWithdrawalValidator>(
+    await validatorFactory.deploy()
+  );
+
+  const VaultFactory: MammonVaultV0__factory = await ethers.getContractFactory(
+    "MammonVaultV0",
+    {
+      libraries: {
+        "contracts/libraries/SmartPoolManager.sol:SmartPoolManager":
+          config.poolManager,
+      },
+    },
+  );
+  const vault = <MammonVaultV0>(
+    await VaultFactory.deploy(
+      config.bFactory,
+      token0,
+      token1,
+      manager,
+      validator.address,
+      DEFAULT_NOTICE_PERIOD,
+    )
+  );
+  console.log("Vault deployed to: ", vault.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere and properly handle errors.
