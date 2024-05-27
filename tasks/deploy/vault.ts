cfad0285178e879f886255da508b212b10062580
Louis Brown <louisbrown0212@gmail.com>
2024-05-26 20:14:45 -0400

    fix conflicts of deploy in package.json

--- tasks/deploy/vault.ts
+++ tasks/deploy/vault.ts
@@ -1,30 +1,18 @@
-import { DEFAULT_NOTICE_PERIOD, getConfig } from "../../scripts/config";
+import { getConfig } from "../../scripts/config";
 import { task } from "hardhat/config";
 
 task("deploy:vault")
-  .addOptionalParam("token0", "Token0's address")
-  .addOptionalParam("token1", "Token1's address")
-  .addOptionalParam("manager", "Manager's address")
-  .addOptionalParam("validator", "Validator's address")
+  .addParam("token0", "Token0's address")
+  .addParam("token1", "Token1's address")
+  .addParam("manager", "Manager's address")
+  .addParam("validator", "Validator's address")
+  .addParam("noticePeriod", "Notice Period in second")
   .setAction(async (taskArgs, { deployments, ethers }) => {
     const token0 = taskArgs.token0;
     const token1 = taskArgs.token1;
-    const manager = taskArgs.manager || ethers.constants.AddressZero;
-    const validator =
-      taskArgs.validator || (await deployments.get("Validator"))?.address;
-
-    if (!token0) {
-      console.log("--token0 parameter is not specified");
-      return;
-    }
-    if (!token1) {
-      console.log("--token1 parameter is not specified");
-      return;
-    }
-    if (!validator) {
-      console.log("--validator parameter is not specified");
-      return;
-    }
+    const manager = taskArgs.manager;
+    const validator = taskArgs.validator;
+    const noticePeriod = taskArgs.noticePeriod;
 
     const chainId = (await ethers.provider.getNetwork()).chainId;
     const config = getConfig(chainId);
@@ -36,6 +24,7 @@ task("deploy:vault")
     console.log(`Token1: ${token1}`);
     console.log(`Manager: ${manager}`);
     console.log(`Validator: ${validator}`);
+    console.log(`Notice Period: ${noticePeriod}`);
 
     await deployments.deploy("MammonVaultV0", {
       contract: "MammonVaultV0",
@@ -45,7 +34,7 @@ task("deploy:vault")
         token1,
         manager,
         validator,
-        DEFAULT_NOTICE_PERIOD,
+        noticePeriod,
       ],
       libraries: {
         SmartPoolManager: config.poolManager,
@@ -55,7 +44,7 @@ task("deploy:vault")
     });
 
     console.log(
-      "Vault is deployed to: ",
+      "Vault is deployed to:",
       (await deployments.get("MammonVaultV0")).address,
     );
   });
