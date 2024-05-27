87c58ce6091aeaf4e32f7811ef80a30a6cac01db
Louis Brown <louisbrown0212@gmail.com>
2024-05-27 09:02:20 -0400

    add and update tests with kovan

--- test/integration/BFactory.ts
+++ test/integration/BFactory.ts
@@ -1,35 +1,28 @@
 import { BigNumber } from "@ethersproject/bignumber";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signers";
 import { ethers } from "hardhat";
-import {
-  IBFactory,
-  IBFactory__factory,
-  IERC20,
-  IERC20__factory,
-} from "../../typechain";
+import { setupTokens } from "../fixtures";
+import { getConfig, getChainId } from "../../scripts/config";
+import { IBFactory, IBFactory__factory, IERC20 } from "../../typechain";
 
 let admin: SignerWithAddress;
 let user: SignerWithAddress;
 let bFactory: IBFactory;
-let weth: IERC20, dai: IERC20;
+let WETH: IERC20, DAI: IERC20;
 const UINT256_MAX: BigNumber = BigNumber.from(2).pow(256).sub(1);
 
 describe("Factory", function () {
   before(async function () {
+    const chainId = getChainId(process.env.HARDHAT_FORK);
+    const config = getConfig(chainId);
+
     ({ admin, user } = await ethers.getNamedSigners());
     bFactory = IBFactory__factory.connect(
-      "0x9424B1412450D0f8Fc2255FAf6046b98213B76Bd", // BFactory on mainnet
+      config.bFactory, // BFactory on mainnet
       admin,
     );
 
-    weth = IERC20__factory.connect(
-      "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2", // WETH on mainnet
-      admin,
-    );
-    dai = IERC20__factory.connect(
-      "0x6B175474E89094C44Da98b954EedeAC495271d0F", // DAI on mainnet
-      admin,
-    );
+    ({ DAI, WETH } = await setupTokens());
   });
 
   it("should deploy balancer private pool", async function () {
@@ -42,10 +35,10 @@ describe("Factory", function () {
     );
     const POOL = `0x${LOG_NEW_POOL?.topics[2].slice(26)}`;
 
-    await weth.connect(admin).approve(POOL, UINT256_MAX);
-    await dai.connect(admin).approve(POOL, UINT256_MAX);
+    await WETH.connect(admin).approve(POOL, UINT256_MAX);
+    await DAI.connect(admin).approve(POOL, UINT256_MAX);
 
-    await weth.connect(user).approve(POOL, UINT256_MAX);
-    await dai.connect(user).approve(POOL, UINT256_MAX);
+    await WETH.connect(user).approve(POOL, UINT256_MAX);
+    await DAI.connect(user).approve(POOL, UINT256_MAX);
   });
 });
