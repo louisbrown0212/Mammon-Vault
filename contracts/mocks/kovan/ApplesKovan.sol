d0ec0f9a28e4a01a54547158f1580dc7775866eb
Louis Brown <louisbrown0212@gmail.com>
2024-05-29 06:04:05 -0400

    feat: add kovan test tokens
    **Features**:
    - Add two mintable test tokens for kovan using OZ minter pauser preset
    
    **Misc**:
    - Several dependencies added
    - Run Prettier on codebase
    - Created `constants.ts` to hold `ONE_TOKEN` and future reusable constants

---
+++ contracts/mocks/kovan/ApplesKovan.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.7;
+
+import "../../dependencies/openzeppelin/ERC20PresetMinterPauser.sol";
+
+/// @notice Mock token to be used in Kovan Balancer pools as token0.
+/// @dev THIS CONTRACT IS FOR TESTING PURPOSES ONLY. DO NOT USE IN PRODUCTION.
+contract ApplesKovan is ERC20PresetMinterPauser {
+    // solhint-disable no-empty-blocks
+    constructor() ERC20PresetMinterPauser("Apples", "APPLZ") {}
+}
