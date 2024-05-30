bb12c3ca859a1dc1e393ddb7679af04b4f191fdd
Louis Brown <louisbrown0212@gmail.com>
2024-05-30 05:26:53 -0400

    add ERC165.sol

--- contracts/PermissiveWithdrawalValidator.sol
+++ contracts/PermissiveWithdrawalValidator.sol
@@ -1,23 +1,25 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.7;
 
-import "./dependencies/openzeppelin/IERC165.sol";
+import "./dependencies/openzeppelin/ERC165.sol";
 import { IWithdrawalValidator } from "./interfaces/IWithdrawalValidator.sol";
 
 /// @notice A withdrawal validator that validates withdrawals of an arbitrary size.
-contract PermissiveWithdrawalValidator is IERC165, IWithdrawalValidator {
+contract PermissiveWithdrawalValidator is ERC165, IWithdrawalValidator {
     uint256 public constant ANY_AMOUNT = type(uint256).max;
 
     function allowance() external pure override returns (uint256, uint256) {
         return (ANY_AMOUNT, ANY_AMOUNT);
     }
 
-    function supportsInterface(bytes4 interfaceID)
-        external
+    function supportsInterface(bytes4 interfaceId)
+        public
         view
         override
         returns (bool)
     {
-        return interfaceID == type(IWithdrawalValidator).interfaceId;
+        return
+            interfaceId == type(IWithdrawalValidator).interfaceId ||
+            super.supportsInterface(interfaceId);
     }
 }
