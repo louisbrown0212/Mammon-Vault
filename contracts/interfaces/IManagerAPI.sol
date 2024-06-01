f528a66dbae2c8ed1f20013647777833a47f5925
Louis Brown <louisbrown0212@gmail.com>
2024-06-01 16:11:20 +1000

    update comments of interfaces(o2)

--- contracts/interfaces/IManagerAPI.sol
+++ contracts/interfaces/IManagerAPI.sol
@@ -3,11 +3,17 @@ pragma solidity 0.8.7;
 
 /// @dev This code is not audited or tested. Please do not use in production.
 interface IManagerAPI {
+    /// @notice Emitted when public swap is turned on/off.
+    /// @param value New state of public swap.
     function setPublicSwap(bool value) external;
 
-    /**
-     * @dev Start moving weights gradually to target value.
-     */
+    /// @notice Set target weights of tokens and update period.
+    /// @dev Available only to the manager. Available only if the vault is initialized.
+    ///      Vault shouldn't be on finalizing.
+    /// @param weight0 The target weight of the first token.
+    /// @param weight1 The target weight of the second token.
+    /// @param startBlock The block number that update starts.
+    /// @param endBlock The block number that weights reach out target.
     function updateWeightsGradually(
         uint256 weight0,
         uint256 weight1,
@@ -15,10 +21,12 @@ interface IManagerAPI {
         uint256 endBlock
     ) external;
 
-    /**
-     * @dev Move underlying Balancer weights if updating gradually
-     */
+    /// @notice Update weights according to plan.
+    /// @dev Available only to the manager. Available only if the vault is initialized.
+    ///      Vault shouldn't be on finalizing.
     function pokeWeights() external;
 
+    /// @notice Set swap fee.
+    /// @dev Available only to the manager.
     function setSwapFee(uint256 newSwapFee) external;
 }
