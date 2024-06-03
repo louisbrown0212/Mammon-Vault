f746c5cf600a54d823982bb40ee674471d7d23fd
Louis Brown <louisbrown0212@gmail.com>
2024-06-03 02:11:20 -0400

    update comments of interfaces(o2)

--- contracts/interfaces/IMammonVaultV0.sol
+++ contracts/interfaces/IMammonVaultV0.sol
@@ -11,10 +11,15 @@ import "./IBinaryVault.sol";
  *      This code is not audited or tested. Please do not use in production.
  */
 interface IMammonVaultV0 is IProtocolAPI, IBinaryVault {
+    /// @notice The state of public swap if it's turned on or off.
+    /// @return If public swap is turned on, returns true, otherwise false.
     function isPublicSwap() external view returns (bool);
 
+    /// @notice The swap fee.
     function getSwapFee() external view returns (uint256);
 
+    /// @notice The weight of a token.
+    /// @return The weight of a given token on the pool.
     function getDenormalizedWeight(address token)
         external
         view
