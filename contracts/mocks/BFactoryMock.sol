14efa5ed2f16ef14f5934a8d044212c4a811e9ff
Louis Brown <louisbrown0212@gmail.com>
2024-06-03 11:53:46 -0400

    remove ot audited or tested comment

--- contracts/mocks/BFactoryMock.sol
+++ contracts/mocks/BFactoryMock.sol
@@ -3,7 +3,6 @@ pragma solidity 0.8.7;
 
 import "./BPoolMock.sol";
 
-/// @dev This code is not audited or tested. Please do not use in production.
 contract BFactoryMock {
     function newBPool() external returns (address) {
         return address(new BPoolMock());
