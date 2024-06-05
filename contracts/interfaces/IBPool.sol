44cdb444c778ed3391e6603ee76fdae1b5997acc
Louis Brown <louisbrown0212@gmail.com>
2024-06-05 11:59:29 +1000

    remove IBPoolMock and update IBPool

--- contracts/interfaces/IBPool.sol
+++ contracts/interfaces/IBPool.sol
@@ -20,6 +20,40 @@ interface IBPool {
 
     function finalize() external;
 
+    function swapExactAmountIn(
+        address tokenIn,
+        uint256 tokenAmountIn,
+        address tokenOut,
+        uint256 minAmountOut,
+        uint256 maxPrice
+    ) external returns (uint256 tokenAmountOut, uint256 spotPriceAfter);
+
+    function swapExactAmountOut(
+        address tokenIn,
+        uint256 maxAmountIn,
+        address tokenOut,
+        uint256 tokenAmountOut,
+        uint256 maxPrice
+    ) external returns (uint256 tokenAmountIn, uint256 spotPriceAfter);
+
+    function calcOutGivenIn(
+        uint256 tokenBalanceIn,
+        uint256 tokenWeightIn,
+        uint256 tokenBalanceOut,
+        uint256 tokenWeightOut,
+        uint256 tokenAmountIn,
+        uint256 swapFee
+    ) external view returns (uint256 tokenAmountOut);
+
+    function calcInGivenOut(
+        uint256 tokenBalanceIn,
+        uint256 tokenWeightIn,
+        uint256 tokenBalanceOut,
+        uint256 tokenWeightOut,
+        uint256 tokenAmountOut,
+        uint256 swapFee
+    ) external view returns (uint256 tokenAmountIn);
+
     function setSwapFee(uint256 swapFee) external;
 
     function setPublicSwap(bool publicSwap) external;
@@ -27,6 +61,11 @@ interface IBPool {
     function isPublicSwap() external view returns (bool);
 
     function getSwapFee() external view returns (uint256);
+
+    function getSpotPrice(address tokenIn, address tokenOut)
+        external
+        view
+        returns (uint256);
 
     function getBalance(address token) external view returns (uint256);
 
