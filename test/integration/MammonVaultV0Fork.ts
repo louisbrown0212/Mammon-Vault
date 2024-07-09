29c8f554af9a27d63c8d02075bdfca436534c708
Louis Brown <louisbrown0212@gmail.com>
2024-07-08 21:51:43 -0400

    update tests for mammon vault v1 deployment

--- test/integration/MammonVaultV0Fork.ts
+++ test/integration/MammonVaultV0Fork.ts
@@ -5,18 +5,163 @@ import {
   IERC20,
   MammonVaultV1Mainnet,
   MammonVaultV1Mainnet__factory,
+  WithdrawalValidatorMock,
+  WithdrawalValidatorMock__factory,
 } from "../../typechain";
 import { setupTokens } from "../fixtures";
 import { deployVault, toWei } from "../utils";
 import { DEFAULT_NOTICE_PERIOD } from "../../scripts/config";
 
-const ONE_TOKEN = toWei("1");
-const MIN_WEIGHT = toWei("1");
+const ONE = toWei("1");
+const MIN_WEIGHT = toWei("0.01");
+const MIN_WEIGHTx50 = toWei("0.5");
+const MIN_SWAP_FEE = toWei("0.000001");
+const MAX_SWAP_FEE = toWei("0.1");
+const NOTICE_PERIOD = 10000;
+const MAX_NOTICE_PERIOD = 5184000; // 60 days in seconds
 
-describe("Mammon Vault v0 Mainnet", function () {
+describe("Mammon Vault V1 Mainnet Deployment", function () {
+  let admin: SignerWithAddress;
+  let manager: SignerWithAddress;
+  let validator: WithdrawalValidatorMock;
+  let DAI: IERC20;
+  let WETH: IERC20;
+
+  it("should be reverted to deploy vault", async () => {
+    ({ admin, manager } = await ethers.getNamedSigners());
+
+    ({ DAI, WETH } = await setupTokens());
+
+    await deployments.deploy("Validator", {
+      contract: "WithdrawalValidatorMock",
+      from: admin.address,
+      log: true,
+    });
+    validator = WithdrawalValidatorMock__factory.connect(
+      (await deployments.get("Validator")).address,
+      admin,
+    );
+
+    await deployments.deploy("InvalidValidator", {
+      contract: "InvalidValidatorMock",
+      from: admin.address,
+      log: true,
+    });
+
+    await expect(
+      deployVault(
+        admin,
+        "Test",
+        "TEST",
+        [DAI.address, WETH.address, WETH.address],
+        [MIN_WEIGHTx50.toString(), MIN_WEIGHTx50.toString()],
+        MIN_SWAP_FEE.toString(),
+        ONE.toString(),
+        manager.address,
+      ),
+    ).to.be.revertedWith("Mammon__LengthIsNotSame");
+    await expect(
+      deployVault(
+        admin,
+        "Test",
+        "TEST",
+        [DAI.address, WETH.address],
+        [MIN_WEIGHTx50.toString(), MIN_WEIGHTx50.toString()],
+        MIN_SWAP_FEE.toString(),
+        ONE.toString(),
+        manager.address,
+        validator.address,
+        MAX_NOTICE_PERIOD + 1,
+      ),
+    ).to.be.revertedWith("Mammon__NoticePeriodIsAboveMax");
+    await expect(
+      deployVault(
+        admin,
+        "Test",
+        "TEST",
+        [DAI.address, WETH.address],
+        [MIN_WEIGHTx50.toString(), MIN_WEIGHTx50.toString()],
+        MIN_SWAP_FEE.toString(),
+        ONE.toString(),
+        manager.address,
+        manager.address,
+      ),
+    ).to.be.revertedWith("Mammon__ValidatorIsNotValid");
+    await expect(
+      deployVault(
+        admin,
+        "Test",
+        "TEST",
+        [DAI.address, WETH.address],
+        [MIN_WEIGHTx50.toString(), MIN_WEIGHTx50.toString()],
+        MIN_SWAP_FEE.toString(),
+        ONE.toString(),
+        manager.address,
+        (
+          await deployments.get("InvalidValidator")
+        ).address,
+      ),
+    ).to.be.revertedWith("Mammon__ValidatorIsNotValid");
+    await expect(
+      deployVault(
+        admin,
+        "Test",
+        "TEST",
+        [DAI.address, WETH.address],
+        [MIN_WEIGHTx50.toString(), MIN_WEIGHTx50.toString()],
+        ONE.toString(),
+        ONE.toString(),
+        manager.address,
+        validator.address,
+      ),
+    ).to.be.revertedWith("BAL#202"); // MAX_SWAP_FEE_PERCENTAGE
+    await expect(
+      deployVault(
+        admin,
+        "Test",
+        "TEST",
+        [DAI.address, WETH.address],
+        [MIN_WEIGHTx50.toString(), MIN_WEIGHTx50.toString()],
+        MIN_SWAP_FEE.div(2).toString(),
+        ONE.toString(),
+        manager.address,
+        validator.address,
+      ),
+    ).to.be.revertedWith("BAL#203"); // MIN_SWAP_FEE_PERCENTAGE
+    await expect(
+      deployVault(
+        admin,
+        "Test",
+        "TEST",
+        [DAI.address, WETH.address],
+        [MIN_WEIGHT.toString(), MIN_WEIGHT.toString()],
+        MIN_SWAP_FEE.toString(),
+        ONE.toString(),
+        manager.address,
+        validator.address,
+      ),
+    ).to.be.revertedWith("BAL#308"); // NORMALIZED_WEIGHT_INVARIANT
+    await expect(
+      deployVault(
+        admin,
+        "Test",
+        "TEST",
+        [DAI.address, WETH.address],
+        [MIN_WEIGHTx50.toString(), MIN_WEIGHTx50.toString()],
+        MIN_SWAP_FEE.toString(),
+        ONE.add(1).toString(),
+        manager.address,
+        validator.address,
+      ),
+    ).to.be.revertedWith("BAL#338"); // MAX_MANAGEMENT_SWAP_FEE_PERCENTAGE
+  });
+});
+
+describe("Mammon Vault V1 Mainnet Functionality", function () {
   let admin: SignerWithAddress;
   let manager: SignerWithAddress;
   let vault: MammonVaultV1Mainnet;
+  let validator: WithdrawalValidatorMock;
   let DAI: IERC20;
   let WETH: IERC20;
   let snapshot: unknown;
@@ -32,50 +177,23 @@ describe("Mammon Vault v0 Mainnet", function () {
       from: admin.address,
       log: true,
     });
-
-    await deployments.deploy("InvalidValidator", {
-      contract: "InvalidValidatorMock",
-      from: admin.address,
-      log: true,
-    });
-    await expect(
-      deployVault(
-        admin,
-        "Test",
-        "TEST",
-        [DAI.address, WETH.address],
-        [MIN_WEIGHT.toString(), MIN_WEIGHT.toString()],
-        ONE_TOKEN.toString(),
-        ONE_TOKEN.toString(),
-        manager.address,
-        manager.address,
-      ),
-    ).to.be.revertedWith("Mammon__ValidatorIsNotValid");
-    await expect(
-      deployVault(
-        admin,
-        "Test",
-        "TEST",
-        [DAI.address, WETH.address],
-        [MIN_WEIGHT.toString(), MIN_WEIGHT.toString()],
-        ONE_TOKEN.toString(),
-        ONE_TOKEN.toString(),
-        manager.address,
-        (
-          await deployments.get("InvalidValidator")
-        ).address,
-      ),
-    ).to.be.revertedWith("Mammon__ValidatorIsNotValid");
+    validator = WithdrawalValidatorMock__factory.connect(
+      (await deployments.get("Validator")).address,
+      admin,
+    );
 
     await hre.run("deploy:vault", {
       name: "Test",
       symbol: "TEST",
       tokens: [DAI.address, WETH.address].join(","),
-      weights: [MIN_WEIGHT.toString(), MIN_WEIGHT.toString()].join(","),
-      swapFee: ONE_TOKEN.toString(),
-      managementSwapFee: ONE_TOKEN.toString(),
+      weights: [
+        MIN_WEIGHT.mul(40).toString(),
+        MIN_WEIGHT.mul(60).toString(),
+      ].join(","),
+      swapFee: MIN_SWAP_FEE.toString(),
+      managementSwapFee: ONE.toString(),
       manager: manager.address,
-      validator: (await deployments.get("Validator")).address,
+      validator: validator.address,
       noticePeriod: DEFAULT_NOTICE_PERIOD.toString(),
     });
 
@@ -91,16 +209,16 @@ describe("Mammon Vault v0 Mainnet", function () {
 
   describe("when Vault not initialized", () => {
     beforeEach(async () => {
-      await DAI.approve(vault.address, ONE_TOKEN);
-      await WETH.approve(vault.address, ONE_TOKEN);
+      await DAI.approve(vault.address, ONE);
+      await WETH.approve(vault.address, ONE);
     });
 
     it("should be reverted to call functions", async () => {
-      await expect(vault.deposit(ONE_TOKEN, ONE_TOKEN)).to.be.revertedWith(
+      await expect(vault.deposit(ONE, ONE)).to.be.revertedWith(
         "Mammon__VaultNotInitialized",
       );
 
-      await expect(vault.withdraw(ONE_TOKEN, ONE_TOKEN)).to.be.revertedWith(
+      await expect(vault.withdraw(ONE, ONE)).to.be.revertedWith(
         "Mammon__VaultNotInitialized",
       );
 
