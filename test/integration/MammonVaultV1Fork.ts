332af83a12d0244a88893b3f57789b6466d659a8
Louis Brown <louisbrown0212@gmail.com>
2024-07-26 13:03:41 -0400

    update tests to support multi token test

--- test/integration/MammonVaultV1Fork.ts
+++ test/integration/MammonVaultV1Fork.ts
@@ -11,12 +11,11 @@ import {
   WithdrawalValidatorMock__factory,
 } from "../../typechain";
 import { setupTokens } from "../fixtures";
-import { deployFactory, deployVault, toWei } from "../utils";
+import { deployFactory, deployVault, toWei, valueArray } from "../utils";
 import { DEFAULT_NOTICE_PERIOD } from "../../scripts/config";
 
 const ONE = toWei("1");
 const MIN_WEIGHT = toWei("0.01");
-const MIN_WEIGHTx50 = toWei("0.5");
 const MIN_SWAP_FEE = toWei("0.000001");
 const MAX_SWAP_FEE = toWei("0.1");
 const ZERO_ADDRESS = ethers.constants.AddressZero;
@@ -27,14 +26,14 @@ describe("Mammon Vault V1 Mainnet Deployment", function () {
   let manager: SignerWithAddress;
   let validator: WithdrawalValidatorMock;
   let factory: MammonPoolFactoryV1;
-  let WETH: IERC20;
+  let tokens: IERC20[];
   let sortedTokens: string[];
   let unsortedTokens: string[];
 
   it("should be reverted to deploy vault", async () => {
     ({ admin, manager } = await ethers.getNamedSigners());
 
-    ({ WETH, sortedTokens, unsortedTokens } = await setupTokens());
+    ({ tokens, sortedTokens, unsortedTokens } = await setupTokens());
 
     await deployments.deploy("Validator", {
       contract: "WithdrawalValidatorMock",
@@ -54,14 +53,15 @@ describe("Mammon Vault V1 Mainnet Deployment", function () {
 
     factory = await deployFactory(admin);
 
+    const validWeights = valueArray(ONE.div(tokens.length), tokens.length);
     await expect(
       deployVault(
         admin,
         factory.address,
         "Test",
         "TEST",
-        [...sortedTokens, WETH.address],
-        [MIN_WEIGHTx50.toString(), MIN_WEIGHTx50.toString()],
+        [...sortedTokens, tokens[0].address],
+        validWeights,
         MIN_SWAP_FEE.toString(),
         ONE.toString(),
         manager.address,
@@ -74,7 +74,7 @@ describe("Mammon Vault V1 Mainnet Deployment", function () {
         "Test",
         "TEST",
         sortedTokens,
-        [MIN_WEIGHTx50.toString(), MIN_WEIGHTx50.toString()],
+        validWeights,
         MIN_SWAP_FEE.toString(),
         ONE.toString(),
         manager.address,
@@ -89,7 +89,7 @@ describe("Mammon Vault V1 Mainnet Deployment", function () {
         "Test",
         "TEST",
         sortedTokens,
-        [MIN_WEIGHTx50.toString(), MIN_WEIGHTx50.toString()],
+        validWeights,
         MIN_SWAP_FEE.toString(),
         ONE.toString(),
         manager.address,
@@ -103,7 +103,7 @@ describe("Mammon Vault V1 Mainnet Deployment", function () {
         "Test",
         "TEST",
         sortedTokens,
-        [MIN_WEIGHTx50.toString(), MIN_WEIGHTx50.toString()],
+        validWeights,
         MIN_SWAP_FEE.toString(),
         ONE.toString(),
         manager.address,
@@ -119,7 +119,7 @@ describe("Mammon Vault V1 Mainnet Deployment", function () {
         "Test",
         "TEST",
         unsortedTokens,
-        [MIN_WEIGHTx50.toString(), MIN_WEIGHTx50.toString()],
+        validWeights,
         MIN_SWAP_FEE.toString(),
         ONE.toString(),
         manager.address,
@@ -133,7 +133,7 @@ describe("Mammon Vault V1 Mainnet Deployment", function () {
         "Test",
         "TEST",
         sortedTokens,
-        [MIN_WEIGHTx50.toString(), MIN_WEIGHTx50.toString()],
+        validWeights,
         MAX_SWAP_FEE.add(1).toString(),
         ONE.toString(),
         manager.address,
@@ -147,7 +147,7 @@ describe("Mammon Vault V1 Mainnet Deployment", function () {
         "Test",
         "TEST",
         sortedTokens,
-        [MIN_WEIGHTx50.toString(), MIN_WEIGHTx50.toString()],
+        validWeights,
         MIN_SWAP_FEE.sub(1).toString(),
         ONE.toString(),
         manager.address,
@@ -161,7 +161,7 @@ describe("Mammon Vault V1 Mainnet Deployment", function () {
         "Test",
         "TEST",
         sortedTokens,
-        [MIN_WEIGHT.toString(), MIN_WEIGHT.toString()],
+        valueArray(MIN_WEIGHT, tokens.length),
         MIN_SWAP_FEE.toString(),
         ONE.toString(),
         manager.address,
@@ -175,7 +175,7 @@ describe("Mammon Vault V1 Mainnet Deployment", function () {
         "Test",
         "TEST",
         sortedTokens,
-        [MIN_WEIGHTx50.toString(), MIN_WEIGHTx50.toString()],
+        validWeights,
         MIN_SWAP_FEE.toString(),
         ONE.add(1).toString(),
         manager.address,
@@ -188,33 +188,31 @@ describe("Mammon Vault V1 Mainnet Deployment", function () {
 describe("Mammon Vault V1 Mainnet Functionality", function () {
   let admin: SignerWithAddress;
   let manager: SignerWithAddress;
+  let user: SignerWithAddress;
   let vault: MammonVaultV1Mainnet;
   let validator: WithdrawalValidatorMock;
   let factory: MammonPoolFactoryV1;
-  let DAI: IERC20;
-  let WETH: IERC20;
+  let tokens: IERC20[];
   let sortedTokens: string[];
   let snapshot: unknown;
 
   const getStates = async () => {
-    const holdings0 = await vault.holding(0);
-    const holdings1 = await vault.holding(1);
-    const balance0 = await DAI.balanceOf(admin.address);
-    const balance1 = await WETH.balanceOf(admin.address);
+    const holdings = await Promise.all(tokens.map((_, i) => vault.holding(i)));
+    const balances = await Promise.all(
+      tokens.map(token => token.balanceOf(admin.address)),
+    );
 
     return {
-      holdings0,
-      holdings1,
-      balance0,
-      balance1,
+      holdings,
+      balances,
     };
   };
 
   beforeEach(async function () {
     snapshot = await ethers.provider.send("evm_snapshot", []);
-    ({ admin, manager } = await ethers.getNamedSigners());
+    ({ admin, manager, user } = await ethers.getNamedSigners());
 
-    ({ DAI, WETH, sortedTokens } = await setupTokens());
+    ({ tokens, sortedTokens } = await setupTokens());
 
     await deployments.deploy("Validator", {
       contract: "WithdrawalValidatorMock",
@@ -232,15 +230,13 @@ describe("Mammon Vault V1 Mainnet Functionality", function () {
       admin,
     );
 
+    const validWeights = valueArray(ONE.div(tokens.length), tokens.length);
     await hre.run("deploy:vault", {
       factory: factory.address,
       name: "Test",
       symbol: "TEST",
       tokens: sortedTokens.join(","),
-      weights: [
-        MIN_WEIGHT.mul(40).toString(),
-        MIN_WEIGHT.mul(60).toString(),
-      ].join(","),
+      weights: validWeights.join(","),
       swapFee: MIN_SWAP_FEE.toString(),
       managementSwapFee: ONE.toString(),
       manager: manager.address,
@@ -260,18 +256,19 @@ describe("Mammon Vault V1 Mainnet Functionality", function () {
 
   describe("when Vault not initialized", () => {
     beforeEach(async () => {
-      await DAI.approve(vault.address, ONE);
-      await WETH.approve(vault.address, ONE);
+      for (let i = 0; i < tokens.length; i++) {
+        tokens[i].approve(vault.address, ONE);
+      }
     });
 
     it("should be reverted to call functions", async () => {
-      await expect(vault.deposit([ONE, ONE])).to.be.revertedWith(
-        "Mammon__VaultNotInitialized",
-      );
+      await expect(
+        vault.deposit(valueArray(ONE, tokens.length)),
+      ).to.be.revertedWith("Mammon__VaultNotInitialized");
 
-      await expect(vault.withdraw([ONE, ONE])).to.be.revertedWith(
-        "Mammon__VaultNotInitialized",
-      );
+      await expect(
+        vault.withdraw(valueArray(ONE, tokens.length)),
+      ).to.be.revertedWith("Mammon__VaultNotInitialized");
 
       const blocknumber = await ethers.provider.getBlockNumber();
       await expect(
@@ -295,62 +292,75 @@ describe("Mammon Vault V1 Mainnet Functionality", function () {
     });
 
     it("should be reverted to initialize the vault", async () => {
-      await expect(vault.initialDeposit([ONE.add(1), ONE])).to.be.revertedWith(
-        "ERC20: transfer amount exceeds allowance",
-      );
+      const validAmounts = valueArray(ONE, tokens.length - 1);
 
-      await expect(vault.initialDeposit([ONE, ONE.add(1)])).to.be.revertedWith(
-        "ERC20: transfer amount exceeds allowance",
-      );
+      await expect(
+        vault.initialDeposit([ONE.add(1), ...validAmounts]),
+      ).to.be.revertedWith("ERC20: transfer amount exceeds allowance");
 
-      await expect(vault.initialDeposit([0, ONE])).to.be.revertedWith(
-        "BAL#311",
-      ); // ZERO_INVARIANT
+      await expect(
+        vault.initialDeposit([...validAmounts, ONE.add(1)]),
+      ).to.be.revertedWith("ERC20: transfer amount exceeds allowance");
 
-      await expect(vault.initialDeposit([ONE, 0])).to.be.revertedWith(
-        "BAL#311",
-      ); // ZERO_INVARIANT
+      await expect(
+        vault.initialDeposit([0, ...validAmounts]),
+      ).to.be.revertedWith("BAL#311"); // ZERO_INVARIANT
+
+      await expect(
+        vault.initialDeposit([...validAmounts, 0]),
+      ).to.be.revertedWith("BAL#311"); // ZERO_INVARIANT
     });
 
     it("should be possible to initialize the vault", async () => {
-      const { balance0, balance1 } = await getStates();
+      const { balances } = await getStates();
 
-      expect(await vault.estimateGas.initialDeposit([ONE, ONE])).to.below(
-        500000,
-      );
-      await vault.initialDeposit([ONE, ONE]);
+      expect(
+        await vault.estimateGas.initialDeposit(valueArray(ONE, tokens.length)),
+      ).to.below(800000);
+      await vault.initialDeposit(valueArray(ONE, tokens.length));
 
-      expect(await DAI.balanceOf(admin.address)).to.equal(balance0.sub(ONE));
-      expect(await WETH.balanceOf(admin.address)).to.equal(balance1.sub(ONE));
-      expect(await DAI.balanceOf(await vault.bVault())).to.equal(ONE);
-      expect(await WETH.balanceOf(await vault.bVault())).to.equal(ONE);
+      for (let i = 0; i < tokens.length; i++) {
+        expect(await tokens[i].balanceOf(admin.address)).to.equal(
+          balances[i].sub(ONE),
+        );
+        expect(await tokens[i].balanceOf(await vault.bVault())).to.equal(ONE);
+      }
     });
 
     it("should be reverted to initialize the vault again", async () => {
-      await vault.initialDeposit([ONE, ONE]);
+      await vault.initialDeposit(valueArray(ONE, tokens.length));
 
-      await expect(vault.initialDeposit([ONE, ONE])).to.be.revertedWith(
-        "Mammon__VaultIsAlreadyInitialized",
-      );
+      await expect(
+        vault.initialDeposit(valueArray(ONE, tokens.length)),
+      ).to.be.revertedWith("Mammon__VaultIsAlreadyInitialized");
     });
   });
 
   describe("when Vault is initialized", () => {
     beforeEach(async () => {
-      await DAI.approve(vault.address, toWei(100));
-      await WETH.approve(vault.address, toWei(100));
-      await vault.initialDeposit([ONE, ONE]);
+      for (let i = 0; i < tokens.length; i++) {
+        await tokens[i].approve(vault.address, toWei(100));
+      }
+      await vault.initialDeposit(valueArray(ONE, tokens.length));
     });
 
     describe("when depositing to Vault", () => {
       it("should be reverted to deposit tokens", async () => {
         await expect(
-          vault.deposit([toWei(100), toWei(100)]),
+          vault.connect(user).deposit(valueArray(ONE, tokens.length)),
+        ).to.be.revertedWith("Ownable: caller is not the owner");
+
+        await expect(
+          vault.deposit(valueArray(toWei(100), tokens.length)),
         ).to.be.revertedWith("ERC20: transfer amount exceeds allowance");
       });
 
       it("should be possible to deposit tokens", async () => {
-        await vault.deposit([ONE, ONE]);
+        await vault.deposit(valueArray(ONE, tokens.length));
+      });
+
+      it("should be possible to withdraw tokens", async () => {
+        await vault.withdraw(valueArray(ONE, tokens.length));
       });
     });
   });
