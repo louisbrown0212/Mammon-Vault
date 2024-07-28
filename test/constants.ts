b809b6e310944ee9a6ada347819ee1fbb1791cf3
Louis Brown <louisbrown0212@gmail.com>
2024-07-28 11:58:55 +1000

    moved constant variables into constants script

--- test/constants.ts
+++ test/constants.ts
@@ -1,3 +1,9 @@
 import { toWei } from "./utils";
+import { ethers } from "hardhat";
 
-export const ONE_TOKEN = toWei("1");
+export const ONE = toWei("1");
+export const MIN_WEIGHT = toWei("0.01");
+export const MIN_SWAP_FEE = toWei("0.000001");
+export const MAX_SWAP_FEE = toWei("0.1");
+export const ZERO_ADDRESS = ethers.constants.AddressZero;
+export const MAX_NOTICE_PERIOD = 5184000; // 60 days in seconds
