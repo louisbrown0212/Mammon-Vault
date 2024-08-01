7baa6e327fe5dfb7b8555258275e577620a2f047
Louis Brown <louisbrown0212@gmail.com>
2024-08-01 11:08:47 -0400

    update deployment methods

--- test/fixtures.ts
+++ test/fixtures.ts
@@ -1,60 +1,63 @@
 import { parseEther } from "@ethersproject/units";
-import { deployments } from "hardhat";
-import { ERC20Mock__factory } from "../typechain";
+import { ethers } from "hardhat";
+import { ERC20Mock, ERC20Mock__factory } from "../typechain";
 
-export const setupTokens = deployments.createFixture(
-  async ({ deployments, ethers }) => {
-    const { deploy } = deployments;
-    const { admin } = await ethers.getNamedSigners();
+export const setupTokens = async (): Promise<{
+  tokens: ERC20Mock[];
+  sortedTokens: string[];
+  unsortedTokens: string[];
+}> => {
+  const { admin } = await ethers.getNamedSigners();
 
-    const tokenDeploys = [];
+  const tokenDeploys = [];
+  const erc20 = await ethers.getContractFactory<ERC20Mock__factory>(
+    "ERC20Mock",
+  );
 
-    for (let i = 0; i < 4; i++) {
-      const token = await deploy(`TOKEN${i}`, {
-        contract: "ERC20Mock",
-        from: admin.address,
-        args: [`TOKEN${i} Test`, `TTOKEN${i}`, 18, parseEther("1000000000")],
-      });
-      tokenDeploys.push(token);
+  for (let i = 0; i < 4; i++) {
+    const token = await erc20
+      .connect(admin)
+      .deploy(`TOKEN${i} Test`, `TTOKEN${i}`, 18, parseEther("1000000000"));
+    tokenDeploys.push(token);
+  }
+
+  const tokens = tokenDeploys
+    .map(token => ERC20Mock__factory.connect(token.address, admin))
+    .sort((a, b) => (a.address < b.address ? -1 : 1));
+
+  const sortedTokens = [];
+  const unsortedTokens = [];
+
+  for (let i = 0; i < tokens.length; i += 2) {
+    sortedTokens.push(tokens[i].address);
+    if (i + 1 < tokens.length) {
+      sortedTokens.push(tokens[i + 1].address);
+      unsortedTokens.push(tokens[i + 1].address);
     }
+    unsortedTokens.push(tokens[i].address);
+  }
 
-    const tokens = tokenDeploys
-      .map(token => ERC20Mock__factory.connect(token.address, admin))
-      .sort((a, b) => (a.address < b.address ? -1 : 1));
+  return {
+    tokens,
+    sortedTokens,
+    unsortedTokens,
+  };
+};
 
-    const sortedTokens = [];
-    const unsortedTokens = [];
+export const deployToken = async (): Promise<{
+  TOKEN: ERC20Mock;
+}> => {
+  const { admin } = await ethers.getNamedSigners();
 
-    for (let i = 0; i < tokens.length; i += 2) {
-      sortedTokens.push(tokens[i].address);
-      if (i + 1 < tokens.length) {
-        sortedTokens.push(tokens[i + 1].address);
-        unsortedTokens.push(tokens[i + 1].address);
-      }
-      unsortedTokens.push(tokens[i].address);
-    }
+  const erc20 = await ethers.getContractFactory<ERC20Mock__factory>(
+    "ERC20Mock",
+  );
 
-    return {
-      tokens,
-      sortedTokens,
-      unsortedTokens,
-    };
-  },
-);
+  const token = await erc20
+    .connect(admin)
+    .deploy("TOKEN Test", "TTOKEN", 18, parseEther("1000000000"));
 
-export const deployToken = deployments.createFixture(
-  async ({ deployments, ethers }) => {
-    const { deploy } = deployments;
-    const { admin } = await ethers.getNamedSigners();
-
-    const TOKEN = await deploy("TOKEN", {
-      contract: "ERC20Mock",
-      from: admin.address,
-      args: ["TOKEN Test", "TTOKEN", 18, parseEther("1000000000")],
-    });
-
-    return {
-      TOKEN: ERC20Mock__factory.connect(TOKEN.address, admin),
-    };
-  },
-);
+  return {
+    TOKEN: ERC20Mock__factory.connect(token.address, admin),
+  };
+};
