239f6619e509fdb9bc266ac0bfdf610d0a7fa6ba
Louis Brown <louisbrown0212@gmail.com>
2024-08-01 22:23:35 -0400

    removed some unnecessary functions interfaces

--- contracts/interfaces/IBVault.sol
+++ contracts/interfaces/IBVault.sol
@@ -4,72 +4,6 @@ pragma solidity 0.8.7;
 import "../dependencies/openzeppelin/IERC20.sol";
 
 interface IBVault {
-    function hasApprovedRelayer(address user, address relayer)
-        external
-        view
-        returns (bool);
-
-    function setRelayerApproval(
-        address sender,
-        address relayer,
-        bool approved
-    ) external;
-
-    function getInternalBalance(address user, IERC20[] memory tokens)
-        external
-        view
-        returns (uint256[] memory);
-
-    function manageUserBalance(UserBalanceOp[] memory ops) external payable;
-
-    struct UserBalanceOp {
-        UserBalanceOpKind kind;
-        IERC20 asset;
-        uint256 amount;
-        address sender;
-        address payable recipient;
-    }
-
-    enum UserBalanceOpKind {
-        DEPOSIT_INTERNAL,
-        WITHDRAW_INTERNAL,
-        TRANSFER_INTERNAL,
-        TRANSFER_EXTERNAL
-    }
-
-    enum PoolSpecialization {
-        GENERAL,
-        MINIMAL_SWAP_INFO,
-        TWO_TOKEN
-    }
-
-    function registerPool(PoolSpecialization specialization)
-        external
-        returns (bytes32);
-
-    function getPool(bytes32 poolId)
-        external
-        view
-        returns (address, PoolSpecialization);
-
-    function registerTokens(
-        bytes32 poolId,
-        IERC20[] memory tokens,
-        address[] memory assetManagers
-    ) external;
-
-    function deregisterTokens(bytes32 poolId, IERC20[] memory tokens) external;
-
-    function getPoolTokenInfo(bytes32 poolId, IERC20 token)
-        external
-        view
-        returns (
-            uint256 cash,
-            uint256 managed,
-            uint256 lastChangeBlock,
-            address assetManager
-        );
-
     function getPoolTokens(bytes32 poolId)
         external
         view
@@ -156,13 +90,6 @@ interface IBVault {
         address payable recipient;
         bool toInternalBalance;
     }
-
-    function queryBatchSwap(
-        SwapKind kind,
-        BatchSwapStep[] memory swaps,
-        IERC20[] memory assets,
-        FundManagement memory funds
-    ) external returns (int256[] memory assetDeltas);
 
     function managePoolBalance(PoolBalanceOp[] memory ops) external;
 
