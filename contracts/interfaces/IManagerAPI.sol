0722380b9ec33cab832e272fc444525144441507
Louis Brown <louisbrown0212@gmail.com>
2024-08-12 22:10:45 -0400

    fix conflicts with getNormalizedWeights function

--- contracts/interfaces/IManagerAPI.sol
+++ contracts/interfaces/IManagerAPI.sol
@@ -4,24 +4,19 @@ pragma solidity 0.8.7;
 /// @title Interface for vault manager.
 /// @notice Supports parameter submission.
 interface IManagerAPI {
-    /// @notice Emitted when public swap is turned on/off.
-    /// @param value New state of public swap.
-    function setPublicSwap(bool value) external;
+    /// @notice Enable or disable swap.
+    /// @param value New state of swap.
+    function setSwapEnabled(bool value) external;
 
     /// @notice Initiate weight move to target in given update window.
-    /// @param targetWeight0 Target weight of first token.
-    /// @param targetWeight1 Target weight of second token.
-    /// @param startBlock Block number at which weight movement starts.
-    /// @param endBlock Block number at which the weights should reach target.
+    /// @param targetWeights Target weights of tokens.
+    /// @param startTime Timestamp at when weight movement starts.
+    /// @param endTime Timestamp at when the weights should reach target.
     function updateWeightsGradually(
-        uint256 targetWeight0,
-        uint256 targetWeight1,
-        uint256 startBlock,
-        uint256 endBlock
+        uint256[] memory targetWeights,
+        uint256 startTime,
+        uint256 endTime
     ) external;
-
-    /// @notice Update weights if within update window.
-    function pokeWeights() external;
 
     /// @notice Change swap fee.
     function setSwapFee(uint256 newSwapFee) external;
