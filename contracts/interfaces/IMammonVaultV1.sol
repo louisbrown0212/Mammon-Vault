46145edd67c25a7154855acb581f0228cf16e738
Louis Brown <louisbrown0212@gmail.com>
2024-08-14 11:57:23 -0400

    renamed IBinaryVault to IMultiAssetVault

--- contracts/interfaces/IMammonVaultV1.sol
+++ contracts/interfaces/IMammonVaultV1.sol
@@ -4,10 +4,15 @@ pragma solidity 0.8.7;
 import "./IUserAPI.sol";
 import "./IManagerAPI.sol";
 import "./IProtocolAPI.sol";
-import "./IBinaryVault.sol";
+import "./IMultiAssetVault.sol";
 
 /// @title Interface for v1 vault.
 // solhint-disable-next-line no-empty-blocks
-interface IMammonVaultV1 is IUserAPI, IManagerAPI, IProtocolAPI, IBinaryVault {
+interface IMammonVaultV1 is
+    IUserAPI,
+    IManagerAPI,
+    IProtocolAPI,
+    IMultiAssetVault
+{
 
 }
