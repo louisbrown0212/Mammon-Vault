236cdff8d15ab5b9dd0ecc3af0b234250c733d9d
Louis Brown <louisbrown0212@gmail.com>
2024-08-26 09:03:17 -0400

    added comments in MammonPoolFactoryV1 interface

--- contracts/interfaces/IMammonPoolFactoryV1.sol
+++ contracts/interfaces/IMammonPoolFactoryV1.sol
@@ -5,6 +5,17 @@ import "../dependencies/openzeppelin/IERC20.sol";
 import "./IBVault.sol";
 
 interface IMammonPoolFactoryV1 {
+    /// @notice Deploys a New Balancer ManagedPool.
+    /// @param name Pool Token name.
+    /// @param symbol Pool Token symbol.
+    /// @param tokens Addresses of asset tokens.
+    /// @param weights Initial weights of asset tokens.
+    /// @param assetManagers Addresses of asset managers.
+    /// @param swapFeePercentage Swap fee percentage.
+    /// @param owner Pool controller.
+    /// @param swapEnabledOnStart Swap enabled states on start.
+    /// @param mustAllowlistLPs If true, only listed addresses can join the pool.
+    /// @param managementSwapFeePercentage Management swap fee percentage.
     function create(
         string memory name,
         string memory symbol,
@@ -18,5 +29,7 @@ interface IMammonPoolFactoryV1 {
         uint256 managementSwapFeePercentage
     ) external returns (address);
 
+    /// @notice Balancer vault
+    /// @return Balancer vault for balancer pool
     function getVault() external view returns (IBVault);
 }
