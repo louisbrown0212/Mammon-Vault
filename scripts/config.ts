3c9f71c3f83c8a361b1248a052fbe18bd179abc1
Louis Brown <louisbrown0212@gmail.com>
2024-08-29 17:33:50 -0400

    Support polygon deployments and testing (#194)

--- scripts/config.ts
+++ scripts/config.ts
@@ -1,24 +1,15 @@
-// Addresses are taken from https://docs.balancer.fi/v/v1/smart-contracts/addresses
-export const getConfig = (
-  chainId: number,
-): { vault: string; factory: string; bVault: string } => {
+import { chainIds } from "../hardhat.config";
+
+// Addresses are taken from https://dev.balancer.fi/references/contracts/contract-addresses
+export const getConfig = (chainId: number): { bVault: string } => {
   switch (chainId) {
-    case 1:
+    case chainIds.mainnet:
+    case chainIds.polygon:
+    case chainIds.kovan:
+    case chainIds.hardhat:
+    case chainIds.rinkeby:
+    case chainIds.goerli:
       return {
-        vault: "MammonVaultV1",
-        factory: "0x48767F9F868a4A7b86A90736632F6E44C2df7fa9",
-        bVault: "0xBA12222222228d8Ba445958a75a0704d566BF2C8",
-      };
-    case 42:
-      return {
-        vault: "MammonVaultV1",
-        factory: "0xb08E16cFc07C684dAA2f93C70323BAdb2A6CBFd2",
-        bVault: "0xBA12222222228d8Ba445958a75a0704d566BF2C8",
-      };
-    case 31337:
-      return {
-        vault: "MammonVaultV1",
-        factory: "0x48767F9F868a4A7b86A90736632F6E44C2df7fa9",
         bVault: "0xBA12222222228d8Ba445958a75a0704d566BF2C8",
       };
     default:
@@ -26,25 +17,10 @@ export const getConfig = (
   }
 };
 
-export const getChainId = (network: string | undefined): number => {
-  switch (network) {
-    case "ganache":
-      return 1337;
-    case "goerli":
-      return 5;
-    case "hardhat":
-      return 31337;
-    case "kovan":
-      return 42;
-    case "mainnet":
-      return 1;
-    case "rinkeby":
-      return 4;
-    case "ropsten":
-      return 3;
-    default:
-      return 31337;
-  }
+export const getChainId = (network?: string): number => {
+  return network
+    ? chainIds[network as keyof typeof chainIds]
+    : chainIds.hardhat;
 };
 
 export const DEFAULT_NOTICE_PERIOD = 3600;
