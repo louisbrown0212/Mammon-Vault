215fd54cf31e8cb6a7271a8da2b2f45f387d3358
Louis Brown <louisbrown0212@gmail.com>
2024-09-01 06:14:26 -0400

    Add optimizer settings as in Balancer repo

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -167,10 +167,27 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
+        // these settings are for Balancer contracts
         version: "0.8.1",
+        settings: {
+          optimizer: {
+            enabled: true,
+            // ref: https://github.com/balancer-labs/balancer-v2-monorepo/blob/3caf66978d3e5f3bb2af050bd8131983c83d9844/pvt/common/hardhat-base-config.ts#L48
+            runs: 9999,
+          },
+        },
       },
       {
+        // these settings are for Mammon contracts
         version: "0.8.11",
+        settings: {
+          // You should disable the optimizer when debugging
+          // https://hardhat.org/hardhat-network/#solidity-optimizer-support
+          optimizer: {
+            enabled: true,
+            runs: 800,
+          },
+        },
       },
     ],
     settings: {
@@ -178,12 +195,6 @@ const config: HardhatUserConfig = {
         // Not including the metadata hash
         // https://github.com/paulrberg/solidity-template/issues/31
         bytecodeHash: "none",
-      },
-      // You should disable the optimizer when debugging
-      // https://hardhat.org/hardhat-network/#solidity-optimizer-support
-      optimizer: {
-        enabled: true,
-        runs: 800,
       },
     },
   },
