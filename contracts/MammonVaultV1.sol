5ef21be293223f4652d032967930df968626f2ce
Louis Brown <louisbrown0212@gmail.com>
2024-09-07 10:23:46 -0400

    remove Mammon__CallerIsNotOwnerOrManager error

--- contracts/MammonVaultV1.sol
+++ contracts/MammonVaultV1.sol
@@ -147,7 +147,6 @@ contract MammonVaultV1 is IMammonVaultV1, Ownable, ReentrancyGuard {
     );
     error Mammon__ValidatorIsNotValid(address validator);
     error Mammon__NoticePeriodIsAboveMax(uint256 actual, uint256 max);
-    error Mammon__CallerIsNotOwnerOrManager();
     error Mammon__NoticeTimeoutNotElapsed(uint64 noticeTimeoutAt);
     error Mammon__ManagerIsZeroAddress();
     error Mammon__CallerIsNotManager();
