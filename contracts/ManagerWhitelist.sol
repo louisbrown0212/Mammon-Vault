c5721fea8230fd3737229eb708c7a4f80cbe73c7
Louis Brown <louisbrown0212@gmail.com>
2024-10-16 21:47:03 -0400

    add IManagerWhitelist and ManagerWhitelist

---
+++ contracts/ManagerWhitelist.sol
@@ -0,0 +1,92 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.11;
+
+import "./dependencies/openzeppelin/Ownable.sol";
+import "./dependencies/openzeppelin/EnumerableSet.sol";
+import "./interfaces/IManagerWhitelist.sol";
+
+/// @title Manager whitelist management.
+/// @notice ManagerWhitelist contract that manages manager list.
+contract ManagerWhitelist is Ownable, IManagerWhitelist {
+    using EnumerableSet for EnumerableSet.AddressSet;
+
+    /// STORAGE SLOT START ///
+
+    /// @notice Manager list.
+    EnumerableSet.AddressSet private managers;
+
+    /// EVENTS ///
+
+    /// @notice Emitted when manager list is initialized.
+    /// @param managers Manager addresses.
+    event ManagerInitialized(address[] managers);
+
+    /// @notice Emitted when a new manager is added.
+    /// @param manager New manager address.
+    event ManagerCreated(address indexed manager);
+
+    /// @notice Emitted when a manager is removed.
+    /// @param manager Removed manager address.
+    event ManagerRemoved(address indexed manager);
+
+    /// ERRORS ///
+
+    error ManagerIsZeroAddress();
+    error AddressIsNotManager();
+
+    /// FUNCTIONS ///
+
+    /// @notice Initialize the contract by initializing manager list.
+    /// @param managers_ Manager addresses.
+    constructor(address[] memory managers_) {
+        if (managers_.length > 0) {
+            for (uint256 i = 0; i < managers_.length; i++) {
+                if (managers_[i] == address(0)) {
+                    revert ManagerIsZeroAddress();
+                }
+            }
+
+            emit ManagerInitialized(managers_);
+        }
+    }
+
+    /// API ///
+
+    /// @inheritdoc IManagerWhitelist
+    function addManager(address manager) external override onlyOwner {
+        if (manager == address(0)) {
+            revert ManagerIsZeroAddress();
+        }
+
+        managers.add(manager);
+
+        emit ManagerCreated(manager);
+    }
+
+    /// @inheritdoc IManagerWhitelist
+    function removeManager(address manager) external override onlyOwner {
+        if (!managers.contains(manager)) {
+            revert AddressIsNotManager();
+        }
+
+        managers.remove(manager);
+
+        emit ManagerRemoved(manager);
+    }
+
+    /// @inheritdoc IManagerWhitelist
+    function isManager(address manager)
+        external
+        view
+        override
+        onlyOwner
+        returns (bool)
+    {
+        return managers.contains(manager);
+    }
+
+    /// @inheritdoc IManagerWhitelist
+    function getManagers() external view override returns (address[] memory) {
+        return managers.values();
+    }
+}
