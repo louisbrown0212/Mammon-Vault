d041bdfe059115c4688e533eb4e52ec197f255b5
Louis Brown <louisbrown0212@gmail.com>
2024-10-18 13:22:52 -0400

    update test for ManagerWhitelist with ManagerWhitelistFactory

--- test/unit/ManagerWhitelist.ts
+++ test/unit/ManagerWhitelist.ts
@@ -1,7 +1,8 @@
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signers";
 import { expect } from "chai";
-import { ethers } from "hardhat";
-import { ManagerWhitelist__factory, ManagerWhitelist } from "../../typechain";
+import hre, { ethers } from "hardhat";
+import { ManagerWhitelist } from "../../typechain";
+import { ManagerWhitelistFactory } from "../../typechain/ManagerWhitelistFactory";
 import { ZERO_ADDRESS } from "../constants";
 import { deployManagerWhitelist } from "../utils";
 
@@ -38,7 +39,8 @@ describe("ManagerWhitelist Functionality", function () {
   let admin: SignerWithAddress;
   let manager: SignerWithAddress;
   let users: SignerWithAddress[];
-  let contract: ManagerWhitelist;
+  let factory: ManagerWhitelistFactory;
+  let managerWhitelist: ManagerWhitelist;
   let snapshot: unknown;
 
   beforeEach(async function () {
@@ -49,47 +51,56 @@ describe("ManagerWhitelist Functionality", function () {
     manager = signers[1];
     users = signers.slice(2);
 
-    const contractFactory =
-      await ethers.getContractFactory<ManagerWhitelist__factory>(
-        "ManagerWhitelist",
-      );
-    contract = await contractFactory.connect(admin).deploy([]);
+    factory = await hre.run("deploy:managerWhitelistFactory", {
+      silent: true,
+    });
+
+    managerWhitelist = await hre.run("deploy:managerWhitelist", {
+      factory: factory.address,
+      salt: "1",
+      silent: true,
+    });
   });
 
   afterEach(async () => {
     await ethers.provider.send("evm_revert", [snapshot]);
   });
 
+  it("ManagerWhitelist should be deployed on precomputed address", async () => {
+    const precomputedAddress = await factory.computeAddress([], 1);
+    expect(managerWhitelist.address).to.be.equal(precomputedAddress);
+  });
+
   describe("Add Manager", () => {
     describe("should be reverted to add a new manager", () => {
       it("when called from non-owner", async () => {
         await expect(
-          contract.connect(manager).addManager(manager.address),
+          managerWhitelist.connect(manager).addManager(manager.address),
         ).to.be.revertedWith("Ownable: caller is not the owner");
       });
 
       it("when a manager is zero address", async () => {
-        await expect(contract.addManager(ZERO_ADDRESS)).to.be.revertedWith(
-          "Mammon__ManagerIsZeroAddress",
-        );
+        await expect(
+          managerWhitelist.addManager(ZERO_ADDRESS),
+        ).to.be.revertedWith("Mammon__ManagerIsZeroAddress");
       });
 
       it("when a manager is already present", async () => {
-        await contract.addManager(manager.address);
-        await expect(contract.addManager(manager.address)).to.be.revertedWith(
-          "Mammon__AddressIsAlreadyManager",
-        );
+        await managerWhitelist.addManager(manager.address);
+        await expect(
+          managerWhitelist.addManager(manager.address),
+        ).to.be.revertedWith("Mammon__AddressIsAlreadyManager");
       });
     });
 
     it("should be possible to add a new manager", async () => {
       for (let i = 0; i < users.length; i++) {
-        expect(await contract.isManager(users[i].address)).to.be.false;
+        expect(await managerWhitelist.isManager(users[i].address)).to.be.false;
 
-        await contract.addManager(users[i].address);
+        await managerWhitelist.addManager(users[i].address);
 
-        expect(await contract.isManager(users[i].address)).to.be.true;
-        expect(await contract.getManagers()).to.be.eql(
+        expect(await managerWhitelist.isManager(users[i].address)).to.be.true;
+        expect(await managerWhitelist.getManagers()).to.be.eql(
           users.slice(0, i + 1).map(user => user.address),
         );
       }
@@ -99,31 +110,31 @@ describe("ManagerWhitelist Functionality", function () {
   describe("Remove Manager", () => {
     beforeEach(async function () {
       for (let i = 0; i < users.length; i++) {
-        await contract.addManager(users[i].address);
+        await managerWhitelist.addManager(users[i].address);
       }
     });
 
     describe("should be reverted to remove a manager", () => {
       it("when called from non-owner", async () => {
         await expect(
-          contract.connect(manager).removeManager(manager.address),
+          managerWhitelist.connect(manager).removeManager(manager.address),
         ).to.be.revertedWith("Ownable: caller is not the owner");
       });
 
       it("when a manager isn't present", async () => {
         await expect(
-          contract.removeManager(manager.address),
+          managerWhitelist.removeManager(manager.address),
         ).to.be.revertedWith("Mammon__AddressIsNotManager");
       });
     });
 
     it("should be possible to remove a manager", async () => {
       for (let i = 0; i < users.length; i++) {
-        expect(await contract.isManager(users[i].address)).to.be.true;
+        expect(await managerWhitelist.isManager(users[i].address)).to.be.true;
 
-        const managers = await contract.getManagers();
+        const managers = await managerWhitelist.getManagers();
 
-        await contract.removeManager(users[i].address);
+        await managerWhitelist.removeManager(users[i].address);
 
         const managerIndex = managers.findIndex(
           (address: string) => address == users[i].address,
@@ -132,8 +143,8 @@ describe("ManagerWhitelist Functionality", function () {
         newManagers[managerIndex] = managers[managers.length - 1];
         newManagers = newManagers.slice(0, managers.length - 1);
 
-        expect(await contract.isManager(users[i].address)).to.be.false;
-        expect(await contract.getManagers()).to.be.eql(newManagers);
+        expect(await managerWhitelist.isManager(users[i].address)).to.be.false;
+        expect(await managerWhitelist.getManagers()).to.be.eql(newManagers);
       }
     });
   });
