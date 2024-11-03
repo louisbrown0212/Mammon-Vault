27e194c8adbbcacb85d214830395f1326595886d
Louis Brown <louisbrown0212@gmail.com>
2024-11-03 06:18:54 -0400

    update ManagerWhitelistFactory deployment with deterministic-deployment-proxy

--- scripts/config.ts
+++ scripts/config.ts
@@ -1,7 +1,12 @@
 import { chainIds } from "../hardhat.config";
 
 // Addresses are taken from https://dev.balancer.fi/references/contracts/contract-addresses
-export const getConfig = (chainId: number): { bVault: string } => {
+export const getConfig = (
+  chainId: number,
+): {
+  bVault: string; // Balancer Vault address
+  deployerProxy: string; // Deterministic deployment proxy address
+} => {
   switch (chainId) {
     case chainIds.mainnet:
     case chainIds.polygon:
@@ -11,6 +16,7 @@ export const getConfig = (chainId: number): { bVault: string } => {
     case chainIds.goerli:
       return {
         bVault: "0xBA12222222228d8Ba445958a75a0704d566BF2C8",
+        deployerProxy: "0x7A0D94F55792C434d74a40883C6ed8545E406D12",
       };
     default:
       throw "unsupported chain ID";
