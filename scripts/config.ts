b7c4cda4b46bb0011e4de2d6f316819a241dabe5
Louis Brown <louisbrown0212@gmail.com>
2024-11-06 19:32:43 +1000

    add comments for gas price and gas limit configuration

--- scripts/config.ts
+++ scripts/config.ts
@@ -1,6 +1,9 @@
 import { chainIds } from "../hardhat.config";
 
 // Addresses are taken from https://dev.balancer.fi/references/contracts/contract-addresses
+// Shouldn't change the gas price and gas limit.
+// Otherwise the deployment address will be changed.
+
 export const getConfig = (
   chainId: number,
 ): {
