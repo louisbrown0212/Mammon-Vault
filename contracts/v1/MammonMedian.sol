7ee26b4cc1289c9865f04963d4f08882678f685b
Louis Brown <louisbrown0212@gmail.com>
2024-11-20 20:28:38 +1000

    add sorted oracle median

--- contracts/v1/MammonMedian.sol
+++ contracts/v1/MammonMedian.sol
@@ -3,8 +3,13 @@ pragma solidity 0.8.11;
 
 import "./dependencies/chainlink/Median.sol";
 import "./dependencies/chainlink/WeightedMedian.sol";
+import "./dependencies/celo-org/SortedLinkedListWithMedian.sol";
 
 contract MammonMedian {
+    using SortedLinkedListWithMedian for SortedLinkedListWithMedian.List;
+
+    SortedLinkedListWithMedian.List private sortedLinkedList;
+
     // solhint-disable-next-line no-empty-blocks
     constructor() {}
 
@@ -21,5 +26,32 @@ contract MammonMedian {
         int256[] memory weights
     ) external pure returns (int256) {
         return WeightedMedian.calculate(list, weights);
+    }
+
+    function updateList(uint256[] calldata list) external {
+        // slither-disable-next-line uninitialized-local
+        uint256 lesserKey;
+        uint256 greaterKey;
+        uint256 len = list.length;
+        for (uint256 i = 0; i < len; i++) {
+            (
+                uint256[] memory keys,
+                uint256[] memory values,
+
+            ) = sortedLinkedList.getElements();
+            for (uint256 j = 0; j < keys.length; j++) {
+                if (values[j] > list[i]) {
+                    greaterKey = keys[j];
+                } else {
+                    lesserKey = keys[j];
+                    break;
+                }
+            }
+            sortedLinkedList.insert(i + 1, list[i], lesserKey, greaterKey);
+        }
+    }
+
+    function calculateSortedLinkedMedian() external view returns (uint256) {
+        return sortedLinkedList.getMedianValue();
     }
 }
