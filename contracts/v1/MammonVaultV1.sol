b5e10701fcf669f0aae3f9e5d67ab2eef1decf71
Louis Brown <louisbrown0212@gmail.com>
2024-12-23 03:38:37 -0400

    fix: remove unused `SafeMath`
    See last comment here: https://github.com/spearbit-audits/mammon/issues/47.

--- contracts/v1/MammonVaultV1.sol
+++ contracts/v1/MammonVaultV1.sol
@@ -19,7 +19,6 @@ import "./interfaces/IWithdrawalValidator.sol";
 ///         in line with a pre-defined validator contract.
 /// @dev Vault owner is the asset owner.
 contract MammonVaultV1 is IMammonVaultV1, Ownable, ReentrancyGuard {
-    using SafeERC20 for IERC20;
     using Math for uint256;
     using SafeCast for uint256;
 
